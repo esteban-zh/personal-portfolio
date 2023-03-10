18a8ce6c9d92687786531b916aa75bc79ed5b887
esteban-zh <eszahe302@gmail.com>
2023-03-10 16:59:23 -0500

    Feat: Adding each project already in production

--- src/components/ProjectCards/ProjectCard.jsx
+++ src/components/ProjectCards/ProjectCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Col } from 'react-bootstrap';
+import { Col, Nav } from 'react-bootstrap';
 import './styles.css';
 
 
-export const ProjectCard = ({ title, description, imgUrl}) => {
+export const ProjectCard = ({ title, description, projectUrl, imgUrl}) => {
   return (
     <Col size={12} sm={6} md={4}>
         <div className="proj-imgbx">
             <img src={imgUrl} />
             <div className="proj-txtx">
-                <h4>{title}</h4>
+                <Nav.Link href={projectUrl}><h4>{title}</h4></Nav.Link>
                 <span>{description}</span>
             </div>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,43 +2,52 @@ import React from 'react'
 import { Tab, Col, Row, Container  } from 'react-bootstrap';
 import { ProjectCard } from '../ProjectCards/ProjectCard';
 import colorSharp2 from "../../assets/img/color-sharp2.png"
-import projImg1 from "../../assets/img/project-img1.png";
-import projImg2 from "../../assets/img/project-img2.png";
-import projImg3 from "../../assets/img/project-img3.png";
+import rentalCarsImg from "../../assets/img/project-rental-cars.jpg";
+import yardSale from "../../assets/img/yard-sale.jpg";
+import tikTok from "../../assets/img/tik-tok.jpg";
+import rickandmorty from "../../assets/img/morty.jpg"
+import twitter from "../../assets/img/tweet.jpg"
+import Automation from "../../assets/img/sistemas-automatizados.jpeg"
 import './styles.css'
 
 export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Buggy and Bumper",
+      description: "App rental cars, landing page using node.js, express, mongoDB, Mongo Atlas cluster, backend deployed in render server, frontend in React.js and deploy in netlify server.",
+      projectUrl:"https://main--candid-croissant-7be055.netlify.app/",
+      imgUrl: rentalCarsImg,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Yard sale",
+      description: "React.js single page application(SPA), using custome Webpack settings. This store works on the client side and queries an API to fetch products.",
+      projectUrl:"https://extraordinary-crostata-113b03.netlify.app/",
+      imgUrl: yardSale,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "tik-tok clone",
+      description: "React.js application cloning the main views of tiktok application",
+      projectUrl:"https://zesty-crepe-6fc049.netlify.app/",
+      imgUrl: tikTok,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Rick and morty API",
+      description: "A simple application deployed through github page using React.js and Bootstrap, consuming Rick and Morty API",
+      projectUrl:"https://esteban-zh.github.io/RickandMortyAPI/",
+      imgUrl: rickandmorty,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Twitter clone",
+      description: "Cloning the home page of twitter application using React.js with Vite, CSS modules",
+      projectUrl:"https://esteban-zh.github.io/twitterProject/",
+      imgUrl: twitter,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "Automation solutions",
+      description: "Web application with Firebase and React.js,  in order to take over each work made daily basis by the team of maintenance area in the Automation solutions company",
+      projectUrl:"https://main--sunny-beignet-216a45.netlify.app/",
+      imgUrl: Automation,
     },
   ];
   
@@ -66,7 +75,7 @@ export const Projects = () => {
                 </Col>
             </Row>
         </Container>
-        <img src={colorSharp2} className="background-image-right" />
+        <img src={colorSharp2} alt="" className="background-image-right" />
     </section>
   )
 }
